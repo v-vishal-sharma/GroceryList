1a660c00018ca08d706c5f66f7039b498faf16c4
Vishal Sharma <vishals200415@gmail.com>
2022-08-28 11:51:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** EVENT LISTENERS *******
+// submit form
+form.addEventListener('submit', addItem);
+
+// ****** FUNCTIONS *************
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString(); //using milliseconds as unique id for item
+    if (value !== '' && editFlag === false){}
+    else if (value !== '' && editFlag === true) {}
+    else {
+        
+    }
+}
+
+// ****** LOCAL STORAGE *********
+
+// ****** SETUP ITEMS ***********
